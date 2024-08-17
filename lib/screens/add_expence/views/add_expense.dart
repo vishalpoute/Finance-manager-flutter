f91592545ac64597b7aa7f402d2cac57feec4ca4
shriy <shriyashpawar2004@gmail.com>
2024-08-17 18:12:09 +0530

    for getting action on + icon

---
+++ lib/screens/add_expence/views/add_expense.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AddExpense extends StatelessWidget {
+  const AddExpense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
